4cd775af5b9d1cc5cea90183b825ac93904be42d
jinwoo <dlwlsdn8022@gmail.com>
2024-12-11 21:06:50 +0900

    controller 에 valid 어노테이션 추가

--- src/main/java/jwlee/springboot_crud/infrastructor/controller/BookRestController.java
+++ src/main/java/jwlee/springboot_crud/infrastructor/controller/BookRestController.java
@@ -1,5 +1,6 @@
 package jwlee.springboot_crud.infrastructor.controller;
 
+import jakarta.validation.Valid;
 import jwlee.springboot_crud.domain.model.Book;
 import jwlee.springboot_crud.application.service.BookService;
 import jwlee.springboot_crud.application.dto.BookReq;
@@ -16,13 +17,13 @@ public class BookRestController {
     private final BookService bookService;
 
     @PostMapping("/book/create")
-    public Book createBook(@RequestBody final BookReq.Create bookReq) {
+    public Book createBook(@RequestBody @Valid final BookReq.Create bookReq) {
         Book saved = bookService.bookCreate(bookReq.toBook());
         return saved;
     }
 
     @PutMapping("/book/edit")
-    public Book updateBook(@RequestBody final BookReq.Update bookReq) {
+    public Book updateBook(@RequestBody @Valid final BookReq.Update bookReq) {
         Book updated = bookService.bookUpdate(bookReq.toBook());
         return updated;
     }
